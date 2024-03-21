ec884ea361f6fa9157e84177d7faf4594476e0fe
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-21 14:16:40 -0500

    Use uncommon pipes to show examples.

--- src/app/products/pages/uncommons-page/uncommons-page.component.ts
+++ src/app/products/pages/uncommons-page/uncommons-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'products-uncommons-page',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class UncommonsPageComponent {
+  // i18nSelect Pipe
+  public name: string = 'Juan Pablo';
+  public gender: 'male' | 'female' = 'male';
 
+  public invitationMap = {
+    'male': 'Mr',
+    'female': 'Mrs'
+  }
+
+  changeClient(): void {
+    this.name = 'Isabela';
+    this.gender = 'female';
+  }
+
+  // i18nPlural and slice Pipes
+  public clients: string[] = [
+    'Maria', 'Juan', 'Pierre', 'Felipe', 'Isabela', 'Pablo'
+  ]
+
+  public clientsMap = {
+    '=0': 'there is no clients',
+    '=1': 'there is a client',
+    'other': 'there are # clients'
+  }
+
+  serveClient(): void {
+    this.clients.shift();
+  }
+
+  // json and keyvalue Pipes
+  public person = {
+    name: 'Juan',
+    age: 28,
+    address: 'Cali, Colombia'
+  }
+
+  // async Pipe
+  public myObservableTimer: Observable<number> = interval(2000)
+    .pipe(
+      tap(value => console.log('tap:', value))
+    );
+
+  public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('The promise got data');
+      console.log('promise')
+    }, 3500);
+  });
 }
